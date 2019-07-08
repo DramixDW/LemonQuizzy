5c50efb610bf9fa97037d175f3ce301a62e314e8
Dramix <romain.verliefden@std.heh.be>
2019-07-08 12:17:00 +0200

    i don't member

--- app/pods/message/user/route.js
+++ app/pods/message/user/route.js
@@ -3,12 +3,21 @@ import adapter from '../../application/adapter';
 
 export default Route.extend({
     model(params){
+        let user = this.store.findRecord('user',params.user_id,{
+            include:'avatar'
+        });
+        this.set('user',user);
         let pool = this.store.query('message',{
-            id : params.user_id
+            id : params.user_id,
+            include:'sender'
         }).then(pool =>{
             return pool;
         })
         return pool;
+    },
+    setupController(controller,model){
+        controller.set('model',model);
+        controller.set('user',this.get('user'));
     }
 });
 
