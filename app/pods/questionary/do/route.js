7297e13cf6a220a4c25dc03c76e04de052a28caa
amaury.deflorenne <amaury.deflorene@std.heh.be>
2019-09-09 23:02:52 +0200

    question-pool nobug

--- app/pods/questionary/do/route.js
+++ app/pods/questionary/do/route.js
@@ -3,9 +3,23 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   async model(params) {
     this.set('QuestionaryID', params.questionary_id);
-    let pool= this.store.createRecord('questionary-pool');
-    pool.set('questionary', this.store.peekRecord('questionary',params.questionary_id));
-    return await pool.save();
+
+    const res = await this.store.peekAll('questionary-pool').filter(async (record) => {
+       let questinary = await record.get('questionary');
+       const id = questinary.get('id');
+       return id == params.questionary_id;
+    });
+
+    if (res.length > 0) {
+      const idPool = await res[0].get('id');
+      const picked = await this.store.peekRecord('questionary-pool',idPool);
+      return picked;
+    }else{
+      let pool= this.store.createRecord('questionary-pool');
+      let questionary = await this.store.findRecord('questionary',params.questionary_id);
+      pool.set('questionary',questionary);
+      return await pool.save();
+    }
   },
   setupController(controller, model) {
     this._super(...arguments);
