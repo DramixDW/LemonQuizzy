b89053a8e30aab4a1f24e3ccc1f8290683cd2601
amaury.deflorenne <amaury.deflorene@std.heh.be>
2019-09-09 13:42:30 +0200

    Progressive Web App :D

--- app/pods/components/navbar/component.js
+++ app/pods/components/navbar/component.js
@@ -3,6 +3,20 @@ import {inject as service} from "@ember/service"
 
 export default Component.extend({
   session: service(),
+  deferredPrompt : null,
+  init() {
+    this._super(...arguments);
+    console.log("init PWA check");
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Stash the event so it can be triggered later.
+      e.preventDefault();
+      this.set('deferredPrompt',e);
+    });
+
+    window.addEventListener('appinstalled', (evt) => {
+      console.log('a2hs installed');
+    });
+  },
   actions: {
     displayMenu() {
       let nav = document.getElementById('nav');
@@ -11,6 +25,18 @@ export default Component.extend({
     },
     logout() {
       this.get('session').invalidate();
+    },
+    installPWA() {
+      this.get('deferredPrompt').prompt();
+      this.get('deferredPrompt').userChoice
+        .then(choiceResult => {
+          if(choiceResult.outcome === 'accepted') {
+            console.log('user accepted A2HS prompt')
+          } else {
+            console.log('user dismissed A2HS prompt')
+          }
+          this.set('deferredPrompt',null);
+        });
     }
   }
 });
