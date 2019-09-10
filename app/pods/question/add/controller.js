333807c8c8e774f760217490e19e1f0368e998d9
Dramix <romain.verliefden@std.heh.be>
2019-09-10 03:19:35 +0200

    color added to questionary

--- app/pods/question/add/controller.js
+++ app/pods/question/add/controller.js
@@ -13,6 +13,10 @@ export default Controller.extend({
       x[i].style.display = display;
     }
   },
+  shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+    return array
+  },
   init() {
     this._super(...arguments);
   },
@@ -30,6 +34,7 @@ export default Controller.extend({
       }
       this.setVisibility(value, 'block');
     },
+   
     addQuizz() {
       let options;
       let answer;
@@ -41,14 +46,22 @@ export default Controller.extend({
           };
           break;
         case "qcm" :
+          let goodAnswer = this.get('answer_qcm').split('\n')
+          let choiceArr = this.get('answer_qcm').split('\n').concat(this.get('bad_answer_qcm').split('\n'))
+          choiceArr = this.shuffle(choiceArr);
+          let indexes = []
+          for(let i=0; i< goodAnswer.length;i++){
+            indexes.push(choiceArr.indexOf(goodAnswer[i]));
+          }
           options = {
-            choices: this.get('answer_qcm').split('\n').concat(this.get('bad_answer_qcm').split('\n'))
+            choices: choiceArr
           };
           answer = {
-            answers: this.get('answer_qcm').split('\n')
+            answers: indexes
           };
+          console.log(options,answer)
           break;
-        case "label" :
+        case "table" :
           break;
         case "text_has_gaps" :
           options = {
@@ -59,27 +72,32 @@ export default Controller.extend({
           };
           break;
         case "math" :
+          //x[1-10]
           options = {
             variables: {
-              "x": [0, 10],
-              "y": [0, 20]
             },
             equation: this.get('formula')
           };
+
+          let unknowns = this.get('unknown').split('\n');
+          for (let val of unknowns) {
+            const [varname,start,end] = val.match(/\w+/g);
+            options.variables[varname] = [parseInt(start),parseInt(end)];
+          }
           answer = {};
+
           break;
       }
       let question_type = this.store.peekRecord('questiontype', this.typeId);
-      let quizz = this.store.peekRecord('questionary',this.get('quizzID'))
       let question = this.store.createRecord("question", {
         title: this.get("title"),
         good_answer_value: parseInt(this.get('good_answer_value')),
         no_answer_value: parseInt(this.get('no_answer_value')),
         bad_answer_value: parseInt(this.get('bad_answer_value')),
         options: options,
-        answer: answer,
+        static_answer: answer,
         questiontype: question_type,
-        quizz : quizz
+        questionary : this.store.peekRecord('questionary',this.get('quizzID'))
       });
       question.save();
     }
